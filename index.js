07382218a578d1fdb88c40a70f325426dd8d62b8
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-06-02 18:28:52 +0300

    fix https

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import http from 'http';
+import https from 'https';
 import Koa from 'koa';
 import koaBody from 'koa-body';
 
@@ -8,4 +8,4 @@ const port = (process.env.NODE_ENV = 'production') ? 80 : 4000;
 app.use(koaBody({ urlencoded: true }));
 app.use(require('./ticket/router').routes());
 
-http.createServer(app.callback()).listen(port);
+https.createServer(app.callback()).listen(port);
