03698cd3dd0ff3a65f1a2dc1cdb8c505568a8725
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-06-02 18:22:49 +0300

    fix port production

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import Koa from 'koa';
 import koaBody from 'koa-body';
 
 const app = new Koa();
+const port = (process.env.NODE_ENV = 'production') ? 80 : 4000;
 
 app.use(koaBody({ urlencoded: true }));
 app.use(require('./ticket/router').routes());
 
-http.createServer(app.callback()).listen(4000);
+http.createServer(app.callback()).listen(port);
