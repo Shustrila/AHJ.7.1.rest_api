0ae84279d327be384397e9f8bc2076936a51c265
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-06-03 10:45:52 +0300

    heroku

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import http from 'http';
 import Koa from 'koa';
 import koaBody from 'koa-body';
 
 const app = new Koa();
+const port = process.env.PORT || 7070;
 
 app.use(koaBody({ urlencoded: true }));
 app.use(require('./ticket/router').routes());
-app.listen(4000);
+
+http.createServer(app.callback()).listen(port);
