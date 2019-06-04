191493bf868adc0c75c612d127731f7b4de1e8bb
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-06-04 12:09:30 +0300

    add headers

--- index.js
+++ index.js
@@ -6,6 +6,15 @@ const app = new Koa();
 const port = process.env.PORT || 7070;
 
 app.use(koaBody({ urlencoded: true }));
+
+app.use(async (ctx, next) => {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, PATCH');
+
+    await next();
+});
+
 app.use(require('./ticket/router').routes());
 
 http.createServer(app.callback()).listen(port);
